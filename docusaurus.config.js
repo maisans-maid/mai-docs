16b0ebb712f07529e3b5564781f3e6e6885dcee6
anujpflash <65192718+anujpflash@users.noreply.github.com>
2020-10-08 15:56:35 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,12 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-    {to: 'About_Me', label: 'About Mai', position: 'left'},
+ {
+          to: 'about/About_Me',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
     
         {
           href: 'https://discord.com/oauth2/authorize?client_id=702074452317307061&scope=bot&permissions=1043721303',
