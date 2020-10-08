f8360238945b809b63757affcf66b62a8ebf4c13
anujpflash <65192718+anujpflash@users.noreply.github.com>
2020-10-08 16:27:45 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ module.exports = {
         },
  {
           to: "About_Me",
-          activeBasePath: '/',
+          activeBasePath: 'about/',
           label: 'About Me',
           position: 'left',
         },
