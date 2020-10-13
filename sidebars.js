5f8ec460f2a181f8646695c13b2db2c7e9c2bde7
BearTS <65192718+BearTS@users.noreply.github.com>
2020-10-14 01:50:36 +0530

    Syntax Error
    Don't know if this will work but this seems to be a syntax error

--- sidebars.js
+++ sidebars.js
@@ -69,9 +69,10 @@ module.exports = {
             'Internal/Classes/PersonalizedCollection'
           ]
         },
-        {
+        
           'Functions',
           'Models',
+        {
           'Data Objects': [
             'Internal/Data Objects/client_config',
             'Internal/Data Objects/command_data',
